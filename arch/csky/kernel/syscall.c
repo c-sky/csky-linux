7d28b2352b3c92a087c7b1ac7b8997519a16d45f
ren_guo <ren_guo@c-sky.com>
2016-10-10 14:09:10 +0800

    csky: bugfix mmap mmap2, and clean up old_select & unistd.h.
    Change-Id: I5a426513d851c5213f52089972c0c2c27d0bddbd

--- arch/csky/kernel/syscall.c
+++ arch/csky/kernel/syscall.c
@@ -1,25 +1,4 @@
-#include <linux/capability.h>
-#include <linux/errno.h>
-#include <linux/sched.h>
-#include <linux/mm.h>
-#include <linux/fs.h>
-#include <linux/smp.h>
-#include <linux/sem.h>
-#include <linux/msg.h>
-#include <linux/shm.h>
-#include <linux/stat.h>
 #include <linux/syscalls.h>
-#include <linux/mman.h>
-#include <linux/file.h>
-#include <linux/utsname.h>
-#include <linux/ipc.h>
-
-#include <asm/setup.h>
-#include <asm/uaccess.h>
-#include <asm/traps.h>
-#include <asm/unistd.h>
-#include <hal/regdef.h>
-#include <asm/user.h>
 
 SYSCALL_DEFINE1(set_thread_area, unsigned long, addr)
 {
@@ -34,44 +13,19 @@ SYSCALL_DEFINE1(set_thread_area, unsigned long, addr)
 	return 0;
 }
 
-/* common code for old and new mmaps */
 SYSCALL_DEFINE6(mmap2,
-	unsigned long, addr, unsigned long, len,
-	unsigned long, prot, unsigned long, flags,
-	unsigned long, fd, unsigned long, pgoff)
+	unsigned long, addr,
+	unsigned long, len,
+	unsigned long, prot,
+	unsigned long, flags,
+	unsigned long, fd,
+	off_t, offset)
 {
-	int error = -EBADF;
-	struct file * file = NULL;
-	unsigned long unused;
-
-	flags &= ~(MAP_EXECUTABLE | MAP_DENYWRITE);
-	if (!(flags & MAP_ANONYMOUS)) {
-		file = fget(fd);
-		if (!file)
-			goto out;
-	}
-
-	down_write(&current->mm->mmap_sem);
-	error = do_mmap_pgoff(file, addr, len, prot, flags, pgoff, &unused);
-	up_write(&current->mm->mmap_sem);
-
-	if (file)
-		fput(file);
-out:
-	return error;
+	if (unlikely(offset & (~PAGE_MASK >> 12)))
+		return -EINVAL;
+	return sys_mmap_pgoff(addr, len, prot, flags, fd,
+		offset >> (PAGE_SHIFT - 12));
 }
-
-SYSCALL_DEFINE0(getpagesize)
-{
-	return PAGE_SIZE;
-}
-
-/*
- * Perform the select(nd, in, out, ex, tv) and mmap() system
- * calls. Linux/csky cloned Linux/i386, which didn't use to be able to
- * handle more than 4 system call parameters, so these system calls
- * used a memory block for parameter passing..
- */
 
 struct mmap_arg_struct {
 	unsigned long addr;
@@ -86,35 +40,13 @@ SYSCALL_DEFINE1(mmap,
 	struct mmap_arg_struct *, arg)
 {
 	struct mmap_arg_struct a;
-	int error = -EFAULT;
 
 	if (copy_from_user(&a, arg, sizeof(a)))
-		goto out;
+		return -EINVAL;
 
-	error = -EINVAL;
-	if (a.offset & ~PAGE_MASK)
-		goto out;
+	if (unlikely(a.offset & ~PAGE_MASK))
+		return -EINVAL;
 
-	a.flags &= ~(MAP_EXECUTABLE | MAP_DENYWRITE);
-
-	error = sys_mmap2(a.addr, a.len, a.prot, a.flags, a.fd, a.offset >> PAGE_SHIFT);
-out:
-	return error;
+	return sys_mmap_pgoff(a.addr, a.len, a.prot, a.flags, a.fd, a.offset >> PAGE_SHIFT);
 }
 
-struct sel_arg_struct {
-	unsigned long n;
-	fd_set *inp, *outp, *exp;
-	struct timeval *tvp;
-};
-
-SYSCALL_DEFINE1(old_select,
-	struct sel_arg_struct *, arg)
-{
-	struct sel_arg_struct a;
-
-	if (copy_from_user(&a, arg, sizeof(a)))
-		return -EFAULT;
-	/* sys_select() does the appropriate kernel locking */
-	return sys_select(a.n, a.inp, a.outp, a.exp, a.tvp);
-}
