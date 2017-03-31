97d85e17fe38af6601ac38b0a1101eb0113cb0e1
Guo Ren <ren_guo@c-sky.com>
2017-03-31 14:42:20 +0800

    csky: use C func call in switch_to.h, instead of asm.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- arch/csky/include/asm/switch_to.h
+++ arch/csky/include/asm/switch_to.h
@@ -1,30 +1,18 @@
 #ifndef __ASM_CSKY_SWITCH_TO_H
 #define __ASM_CSKY_SWITCH_TO_H
 
+#include <linux/thread_info.h>
+
 /*
- * switch_to(n) should switch tasks to task ptr, first checking that
- * ptr isn't the current task, in which case it does nothing.  This
- * also clears the TS-flag if the task we switched to has used the
- * math co-processor latest.
- *
- * syscall stores these registers itself and none of them are used
- * by syscall after the function in the syscall has been called.
- *
- * pass 'prev' in a0, 'next' in a1 and 'last' actually equal to 'prev'.
+ * TODO: we need change switch_to with thread_info,
+ * and we must move pt_regs from thread to thread_info first.
  */
-asmlinkage void resume(void);
-#define switch_to(prev,next,last)				\
-	do {							\
-		register void *_prev __asm__ ("a0") = (prev);	\
-		register void *_next __asm__ ("a1") = (next);	\
-		register void *_last __asm__ ("a0");		\
-		__asm__ __volatile__(				\
-			" jbsr  resume \n\t"			\
-			: "=r"  (_last)				\
-			: "r"   (_prev), "r" (_next)		\
-			: "a2", "a3"				\
-			);					\
-		(last) = _last;					\
-	} while(0)
-#endif
+extern struct task_struct *__switch_to(struct task_struct *, struct task_struct *);
+
+#define switch_to(prev,next,last) \
+do{ \
+	last = __switch_to(prev, next); \
+} while (0)
+
+#endif /*__ASM_CSKY_SWITCH_TO_H */
 
