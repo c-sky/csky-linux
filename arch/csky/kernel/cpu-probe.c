1284de16a85178524d52650d8647aed722e86c07
Guo Ren <ren_guo@c-sky.com>
2017-06-20 19:11:42 +0800

    csky: use the whole register for setting ccr and hint.
    Change-Id: I63c6c56d317af70471cbcd72c6b215c9dbd2e98d
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- arch/csky/kernel/cpu-probe.c
+++ arch/csky/kernel/cpu-probe.c
@@ -3,25 +3,6 @@
 #include <linux/seq_file.h>
 #include <hal/reg_ops.h>
 
-#define CCR_MP		(1 << 0)
-#define CCR_IE		(1 << 2)
-#define CCR_DE		(1 << 3)
-#define CCR_WB		(1 << 4)
-#define CCR_RS		(1 << 5)
-#define CCR_Z		(1 << 6)
-#define CCR_BE		(1 << 7)
-#define CCR_BTB		(1 << 11)
-#define CCR_WA		(1 << 12)
-#define CCR_EV2		(1 << 13)
-#define CCR_WBR		(1 << 14)
-#define CCR_DLE		(1 << 15)
-
-#define HINT_BURST	(1 << 1)
-#define HINT_PLD	(1 << 2)
-#define HINT_MB		(1 << 3)
-#define HINT_LRU	(1 << 4)
-#define HINT_IPLD	(1 << 8)
-
 static struct {
 	u32 ccr;
 	u32 hint;
@@ -32,53 +13,16 @@ char cpu_name[8] = "CKxxx";
 
 static __init void setup_ccr_hint(struct device_node *cpu)
 {
-	cpu_feature.ccr = CCR_MP;
-	cpu_feature.hint = 0;
+	if (of_property_read_u32(cpu, "ccr", &cpu_feature.ccr))
+		return;
 
-	if (of_property_read_bool(cpu, "ccr_ie"))
-		cpu_feature.ccr |= CCR_IE;
+	if (of_property_read_u32(cpu, "hint", &cpu_feature.hint))
+		return;
 
-	if (of_property_read_bool(cpu, "ccr_de"))
-		cpu_feature.ccr |= CCR_DE;
-
-	if (of_property_read_bool(cpu, "ccr_wb"))
-		cpu_feature.ccr |= CCR_WB;
-
-	if (of_property_read_bool(cpu, "ccr_rs"))
-		cpu_feature.ccr |= CCR_RS;
-
-	if (of_property_read_bool(cpu, "ccr_z"))
-		cpu_feature.ccr |= CCR_Z;
-
-	if (of_property_read_bool(cpu, "ccr_be"))
-		cpu_feature.ccr |= CCR_BE;
-
-	if (of_property_read_bool(cpu, "ccr_btb"))
-		cpu_feature.ccr |= CCR_BTB;
-
-	if (of_property_read_bool(cpu, "ccr_wa"))
-		cpu_feature.ccr |= CCR_WA;
-
-	if (of_property_read_bool(cpu, "ccr_wbr"))
-		cpu_feature.ccr |= CCR_WBR;
-
-	if (of_property_read_bool(cpu, "ccr_dle"))
-		cpu_feature.ccr |= CCR_DLE;
-
-	if (of_property_read_bool(cpu, "hint_burst"))
-		cpu_feature.hint |= HINT_BURST;
-
-	if (of_property_read_bool(cpu, "hint_pld"))
-		cpu_feature.hint |= HINT_PLD;
-
-	if (of_property_read_bool(cpu, "hint_mb"))
-		cpu_feature.hint |= HINT_MB;
-
-	if (of_property_read_bool(cpu, "hint_lru"))
-		cpu_feature.hint |= HINT_LRU;
-
-	if (of_property_read_bool(cpu, "hint_ipld"))
-		cpu_feature.hint |= HINT_IPLD;
+	cache_op_all(DATA_CACHE|CACHE_CLR);
+	mtcr_hint(cpu_feature.hint);
+	mtcr_ccr(cpu_feature.ccr);
+	cache_op_all(DATA_CACHE|CACHE_CLR);
 }
 
 static __init void setup_cpu_name(struct device_node *cpu)
@@ -91,11 +35,6 @@ static __init void setup_cpu_name(struct device_node *cpu)
 		sprintf(cpu_name, "CK810");
 	if (of_device_is_compatible(cpu, "csky,ck807"))
 		sprintf(cpu_name, "CK807");
-
-	cache_op_all(DATA_CACHE|CACHE_CLR);
-	mtcr_hint(cpu_feature.hint);
-	mtcr_ccr(cpu_feature.ccr);
-	cache_op_all(DATA_CACHE|CACHE_CLR);
 	return;
 }
 
@@ -115,27 +54,10 @@ __init void cpu_dt_probe(void)
 
 static int c_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "C-SKY CPU revision is: 0x%08x (%s)\n",
-				cpu_feature.cpuid, cpu_name);
-	seq_printf(m, "ccr reg        : 0x%x\n", cpu_feature.ccr);
-	seq_printf(m, "hint reg       : 0x%x\n", cpu_feature.hint);
-	seq_printf(m, "\n");
-	seq_printf(m, "ccr_ie      (2): %s\n", cpu_feature.ccr & CCR_IE    ? "enable":"disabled");
-	seq_printf(m, "ccr_de      (3): %s\n", cpu_feature.ccr & CCR_DE    ? "enable":"disabled");
-	seq_printf(m, "ccr_wb      (4): %s\n", cpu_feature.ccr & CCR_WB    ? "enable":"disabled");
-	seq_printf(m, "ccr_rs      (5): %s\n", cpu_feature.ccr & CCR_RS    ? "enable":"disabled");
-	seq_printf(m, "ccr_z       (6): %s\n", cpu_feature.ccr & CCR_Z     ? "enable":"disabled");
-	seq_printf(m, "ccr_be      (7): %s\n", cpu_feature.ccr & CCR_BE    ? "enable":"disabled");
-	seq_printf(m, "ccr_btb    (11): %s\n", cpu_feature.ccr & CCR_BTB   ? "enable":"disabled");
-	seq_printf(m, "ccr_wa     (12): %s\n", cpu_feature.ccr & CCR_WA    ? "enable":"disabled");
-	seq_printf(m, "ccr_wbr    (14): %s\n", cpu_feature.ccr & CCR_WBR   ? "enable":"disabled");
-	seq_printf(m, "ccr_dle    (15): %s\n", cpu_feature.ccr & CCR_DLE   ? "enable":"disabled");
-	seq_printf(m, "\n");
-	seq_printf(m, "hint_burst  (1): %s\n", cpu_feature.hint& HINT_BURST? "enable":"disabled");
-	seq_printf(m, "hint_pld    (2): %s\n", cpu_feature.hint& HINT_PLD  ? "enable":"disabled");
-	seq_printf(m, "hint_mb     (3): %s\n", cpu_feature.hint& HINT_MB   ? "enable":"disabled");
-	seq_printf(m, "hint_lru    (4): %s\n", cpu_feature.hint& HINT_LRU  ? "enable":"disabled");
-	seq_printf(m, "hint_ipld   (8): %s\n", cpu_feature.hint& HINT_IPLD ? "enable":"disabled");
+	seq_printf(m, "C-SKY CPU : %s\n", cpu_name);
+	seq_printf(m, "revision  : 0x%08x\n", cpu_feature.cpuid);
+	seq_printf(m, "ccr reg   : 0x%08x\n", cpu_feature.ccr);
+	seq_printf(m, "hint reg  : 0x%08x\n", cpu_feature.hint);
 	seq_printf(m, "\n");
 
 	return 0;
