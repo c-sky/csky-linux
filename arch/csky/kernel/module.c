59d499a7536ea63e2d5bce941d11c2c39a5b751c
Guo Ren <ren_guo@c-sky.com>
2017-06-29 11:06:29 +0800

    csky: BUGFIX LC-324 insmod error for CSKY ABIV2.
    Change-Id: I71db4a942ae6fc0a62c15782933313cd7877b784
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- arch/csky/kernel/module.c
+++ arch/csky/kernel/module.c
@@ -9,14 +9,40 @@
 #include <linux/spinlock.h>
 #include <asm/pgtable.h>
 
+#if 0
+#define DEBUGP printk
+#else
 #define DEBUGP(fmt...)
+#endif
+
+#define IS_BSR32(hi16, lo16)        (((hi16) & 0xFC00) == 0xE000)
+#define IS_JSRI32(hi16, lo16)       ((hi16) == 0xEAE0)
+#define CHANGE_JSRI_TO_LRW(addr)    *(uint16_t *)(addr) = (*(uint16_t *)(addr) & 0xFF9F) | 0x0019; \
+							  *((uint16_t *)(addr) + 1) = *((uint16_t *)(addr) + 1) & 0xFFFF
+#define SET_JSR32_R25(addr)         *(uint16_t *)(addr) = 0xE8F9; \
+							  *((uint16_t *)(addr) + 1) = 0x0000;
+
+#ifdef CONFIG_MODULES
 
 void *module_alloc(unsigned long size)
 {
-	if (size == 0)
+#ifdef MODULE_START
+	struct vm_struct *area;
+
+	size = PAGE_ALIGN(size);
+	if (!size)
 		return NULL;
 
+	area = __get_vm_area(size, VM_ALLOC, MODULE_START, MODULE_END);
+	if (!area)
+		return NULL;
+
+	return __vmalloc_area(area, GFP_KERNEL, PAGE_KERNEL);
+#else
+	if (size == 0)
+		return NULL;
 	return vmalloc(size);
+#endif
 }
 
 /*
@@ -29,8 +55,17 @@ void module_free(struct module *mod, void *module_region)
 	   table entries. */
 }
 
+/*
+ * We don't need anything special.
+ */
+int module_frob_arch_sections(Elf_Ehdr *hdr, Elf_Shdr *sechdrs,
+		char *secstrings, struct module *mod)
+{
+	return 0;
+}
+
 int apply_relocate(Elf_Shdr *sechdrs, const char *strtab, unsigned int symindex,
-                unsigned int relsec, struct module *me)
+		unsigned int relsec, struct module *me)
 {
 	unsigned int i;
 	Elf32_Rel *rel = (void *)sechdrs[relsec].sh_addr;
@@ -39,15 +74,15 @@ int apply_relocate(Elf_Shdr *sechdrs, const char *strtab, unsigned int symindex,
 	short * temp;
 
 	DEBUGP("Applying relocate section %u to %u\n", relsec,
-		sechdrs[relsec].sh_info);
+			sechdrs[relsec].sh_info); 
 	for (i = 0; i < sechdrs[relsec].sh_size / sizeof(*rel); i++) {
 		/* This is where to make the change */
 		location = (void *)sechdrs[sechdrs[relsec].sh_info].sh_addr
-		        + rel[i].r_offset;
+			+ rel[i].r_offset;
 		/* This is the symbol it is referring to.  Note that all
 		   undefined symbols have been resolved.  */
 		sym = (Elf32_Sym *)sechdrs[symindex].sh_addr
-		        + ELF32_R_SYM(rel[i].r_info);
+			+ ELF32_R_SYM(rel[i].r_info);
 		switch (ELF32_R_TYPE(rel[i].r_info)) {
 		case R_CSKY_NONE:
 		case R_CSKY_PCRELJSR_IMM11BY2:
@@ -72,7 +107,7 @@ int apply_relocate(Elf_Shdr *sechdrs, const char *strtab, unsigned int symindex,
 			break;
 		default:
 			printk(KERN_ERR "module %s: Unknown relocation: %u\n",
-			       me->name, ELF32_R_TYPE(rel[i].r_info));
+					me->name, ELF32_R_TYPE(rel[i].r_info));
 			return -ENOEXEC;
 		}
 	}
@@ -80,27 +115,27 @@ int apply_relocate(Elf_Shdr *sechdrs, const char *strtab, unsigned int symindex,
 }
 
 int apply_relocate_add(Elf32_Shdr *sechdrs, const char *strtab,
-               unsigned int symindex, unsigned int relsec, struct module *me)
+		unsigned int symindex, unsigned int relsec, struct module *me)
 {
 	unsigned int i;
 	Elf32_Rela *rel = (void *)sechdrs[relsec].sh_addr;
 	Elf32_Sym *sym;
 	uint32_t *location;
 	short * temp;
-#ifdef __CSKYABIV2__
-	uint32_t *nop_location;
+#ifdef CONFIG_CPU_CSKYV2
+	uint16_t *location_tmp;
 #endif
 
 	DEBUGP("Applying relocate_add section %u to %u\n", relsec,
-	       sechdrs[relsec].sh_info);
+			sechdrs[relsec].sh_info);
 	for (i = 0; i < sechdrs[relsec].sh_size / sizeof(*rel); i++) {
 		/* This is where to make the change */
 		location = (void *)sechdrs[sechdrs[relsec].sh_info].sh_addr
-		    + rel[i].r_offset;
+			+ rel[i].r_offset;
 		/* This is the symbol it is referring to.  Note that all
 		   undefined symbols have been resolved.  */
 		sym = (Elf32_Sym *)sechdrs[symindex].sh_addr
-		    + ELF32_R_SYM(rel[i].r_info);
+			+ ELF32_R_SYM(rel[i].r_info);
 
 		switch (ELF32_R_TYPE(rel[i].r_info)) {
 		case R_CSKY_32:
@@ -110,41 +145,48 @@ int apply_relocate_add(Elf32_Shdr *sechdrs, const char *strtab,
 		case R_CSKY_PC32:
 			/* Add the value, subtract its postition */
 			*location = rel[i].r_addend + sym->st_value
-			     - (uint32_t)location;
+				- (uint32_t)location;
 			break;
 		case R_CSKY_PCRELJSR_IMM11BY2:
 			break;
 		case R_CSKY_PCRELJSR_IMM26BY2:
-		/* Relocate jsri */
-#ifdef __CSKYABIV2__
-			*location = (((*location) & 0xffffff9f) | 0x00000019);
-			if (*(location + 1) == 0x4820c400)
-			{
-				*(location + 1) = 0x0000e8f9;
-			}
-			else
-			{
-				/* If the constant pool is between jsri and nop, then we should do this.
-				   The "else{}" can be deleted after the toolchain's optimizion. */
-				nop_location = (uint32_t *)((char*)location + 4 + ((*((short*)(location + 1)) & 0x03ff) << 1));
-				*nop_location = 0x0000e8f9;
+#ifdef CONFIG_CPU_CSKYV2
+			location_tmp = (uint16_t *)location;
+			if (IS_BSR32(*location_tmp, *(location_tmp + 1)))
+				break;
+			else if (IS_JSRI32(*location_tmp, *(location_tmp + 1))) {
+				/* jsri 0x...  --> lrw r25, 0x... */
+				CHANGE_JSRI_TO_LRW(location);
+				/* lsli r0, r0 --> jsr r25 */
+				SET_JSR32_R25(location + 1);
 			}
 #endif
 			break;
 		case R_CSKY_ADDR_HI16:
 			temp = ((short  *)location) + 1;
 			*temp = (short)((rel[i].r_addend + sym->st_value) >> 16);
-			 break;
+			break;
 		case R_CSKY_ADDR_LO16:
 			temp = ((short  *)location) + 1;
 			*temp = (short)((rel[i].r_addend + sym->st_value) & 0xffff);
 			break;
 		default:
 			printk(KERN_ERR "module %s: Unknown relocation: %u\n",
-			       me->name, ELF32_R_TYPE(rel[i].r_info));
+					me->name, ELF32_R_TYPE(rel[i].r_info));
 			return -ENOEXEC;
 		}
 	}
 	return 0;
 }
 
+int module_finalize(const Elf_Ehdr *hdr, const Elf_Shdr *sechdrs,
+		struct module *mod)
+{
+	return 0;
+}
+
+void module_arch_cleanup(struct module *mod)
+{
+}
+
+#endif /* CONFIG_MODULES */
