530a2fb9b435ebfa85d66bfac1f53264d4a16be8
Guo Ren <ren_guo@c-sky.com>
2017-08-17 17:15:34 +0800

    csky: BUGFIX compile error with GCC 6.3
    Change-Id: Ibccac9efe955f51b728adf5e0fbcd75fe39e7e29
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- arch/csky/include/asm/misc_csky.h
+++ arch/csky/include/asm/misc_csky.h
@@ -1,14 +1,14 @@
 #ifndef __ASM_CSKY_MISC_H
 #define __ASM_CSKY_MISC_H
 #include <asm/ptrace.h>
-inline unsigned int read_pt_regs(unsigned int rx, struct pt_regs *regs);
-inline void write_pt_regs(unsigned int value, unsigned int rx, struct pt_regs *regs);
-inline unsigned int read_fpcr(void);
-inline void write_fpcr(unsigned int val);
-inline unsigned int read_fpesr(void);
-inline void write_fpesr(unsigned int val);
+unsigned int read_pt_regs(unsigned int rx, struct pt_regs *regs);
+void write_pt_regs(unsigned int value, unsigned int rx, struct pt_regs *regs);
+unsigned int read_fpcr(void);
+void write_fpcr(unsigned int val);
+unsigned int read_fpesr(void);
+void write_fpesr(unsigned int val);
 #ifdef __CSKYABIV1__
-inline unsigned int read_fpsr(void);
-inline void write_fpsr(unsigned int val);
+unsigned int read_fpsr(void);
+void write_fpsr(unsigned int val);
 #endif
 #endif /* __CSKY_MISC_H__ */
