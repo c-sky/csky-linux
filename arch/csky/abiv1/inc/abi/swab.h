90c3ae18fb07e4c40268245aa396bf3a92625375
Guo Ren <ren_guo@c-sky.com>
2018-03-15 21:30:45 +0800

    csky: add inc/abi/swab.h
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

---
+++ arch/csky/abiv1/inc/abi/swab.h
@@ -0,0 +1,1 @@
+/* nothing to improve with asm */
--- arch/csky/include/uapi/asm/swab.h
+++ arch/csky/include/uapi/asm/swab.h
@@ -1,25 +1,6 @@
 #ifndef __ASM_CSKY_SWAB_H
 #define __ASM_CSKY_SWAB_H
 
-#include <linux/compiler.h>
-#include <linux/types.h>
-
-#ifdef __CSKYABIV2__
-
-static inline __attribute_const__ __u16 __arch_swab16(__u16 x)
-{
-	asm("revh %0, %1\n":"=r" (x):"r"(x));
-	return x;
-}
-#define __arch_swab16 __arch_swab16
-
-static inline __attribute_const__ __u32 __arch_swab32(__u32 x)
-{
-	asm("revb %0, %1\n":"=r"(x):"r"(x));
-	return x;
-}
-#define __arch_swab32 __arch_swab32
-
-#endif /* __CSKYABIV2__ */
+#include <abi/swab.h>
 
 #endif /* __ASM_CSKY_SWAB_H */
