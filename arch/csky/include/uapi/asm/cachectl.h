1292470a70d1f1ea80e65c0480feb8a4c526050b
Guo Ren <ren_guo@c-sky.com>
2018-03-16 11:10:37 +0800

    csky: cleanup cachectl.h
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- arch/csky/include/uapi/asm/cachectl.h
+++ arch/csky/include/uapi/asm/cachectl.h
@@ -2,20 +2,10 @@
 #define __ASM_CSKY_CACHECTL_H
 
 /*
- * Options for cacheflush system call
- *
- * cacheflush() is currently fluch_cache_all().
+ * See "man cacheflush"
  */
-#define ICACHE  (1<<0)      /* flush instruction cache        */
-#define DCACHE  (1<<1)      /* writeback and flush data cache */
-#define BCACHE  (ICACHE|DCACHE) /* flush both caches              */
-
-/*
- * Caching modes for the cachectl(2) call
- *
- * cachectl(2) is currently not supported and returns ENOSYS.
- */
-#define CACHEABLE   0   /* make pages cacheable */
-#define UNCACHEABLE 1   /* make pages uncacheable */
+#define ICACHE  (1<<0)
+#define DCACHE  (1<<1)
+#define BCACHE  (ICACHE|DCACHE)
 
 #endif /* __ASM_CSKY_CACHECTL_H */
