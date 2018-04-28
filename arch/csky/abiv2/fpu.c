5c889666e3649cde950995a67a5f6bbe9e80c416
Guo Ren <ren_guo@c-sky.com>
2018-04-28 20:52:01 +0800

    csky: bugfix fpu_fpe_helper excute mtcr mfcr.
     - pt_regs is wrong number for rx, it will cause get fpc fpesr error.
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- arch/csky/abiv2/fpu.c
+++ arch/csky/abiv2/fpu.c
@@ -2,158 +2,87 @@
 // Copyright (C) 2018 Hangzhou C-SKY Microsystems co.,ltd.
 #include <linux/ptrace.h>
 #include <linux/uaccess.h>
+#include <abi/reg_ops.h>
 
-#define CONFIG_FCR 0
-
-inline unsigned int
-read_pt_regs(unsigned int rx, struct pt_regs *regs)
-{
-	unsigned int value;
-
-	if(rx < 14)
-		value  = *((int *)regs + rx + 3);
-	else if(rx == 14)
-		if(user_mode(regs))
-			asm volatile("mfcr %0, cr<14, 1>\n":"=r"(value));
-		else
-			value = sizeof(struct pt_regs) + ((unsigned int)regs);
-	else
-		value = *((int *)regs + rx + 2);
-
-	return value;
-}
-
-inline void
-write_pt_regs(unsigned int value, unsigned int rx, struct pt_regs *regs)
-{
-	if(rx < 14)
-		*((int *)regs + rx + 3) = value;
-	else if(rx == 14)
-		if(user_mode(regs))
-			asm volatile("mtcr %0, cr<14, 1>\n"::"r"(value));
-		else
-			printk("math emulate trying to write sp.\n");
-	else
-		*((int *)regs + rx + 2) = value;
-}
+#define MTCR_MASK	0xFC00FFE0
+#define MFCR_MASK	0xFC00FFE0
+#define MTCR_DIST	0xC0006420
+#define MFCR_DIST	0xC0006020
 
 void __init init_fpu(void)
 {
-	unsigned long fcr;
-
-	fcr = CONFIG_FCR;
-	asm volatile("mtcr %0, cr<1, 2>\n"::"r"(fcr));
+	mtcr("cr<1, 2>", 0);
 }
 
-inline unsigned int read_fpcr(void)
-{
-	unsigned int result = 0;
-	asm volatile("mfcr %0, cr<1, 2>\n":"=r"(result));
-	return result;
-}
-
-inline void write_fpcr(unsigned int val)
-{
-	unsigned int result = val | CONFIG_FCR;
-	asm volatile("mtcr %0, cr<1, 2>\n"::"r"(result));
-}
-
-inline unsigned int read_fpesr(void)
-{
-	unsigned int result = 0;
-	asm volatile("mfcr %0, cr<2, 2>\n":"=r"(result));
-	return result;
-}
-
-inline void write_fpesr(unsigned int val)
-{
-	unsigned int result = val;
-	asm volatile("mtcr %0, cr<2, 2>\n"::"r"(result));
-}
-
-/* use as fpc control reg read/write in glibc. */
+/*
+ * fpu_libc_helper() is to help libc to excute:
+ *  - mfcr %a, cr<1, 2>
+ *  - mfcr %a, cr<2, 2>
+ *  - mtcr %a, cr<1, 2>
+ *  - mtcr %a, cr<2, 2>
+ */
 int fpu_libc_helper(struct pt_regs * regs)
 {
-	mm_segment_t fs;
+	int fault;
 	unsigned long instrptr, regx = 0;
-	unsigned int fault;
-
-	u16 instr_hi, instr_low;
-	unsigned long index_regx = 0, index_fpregx_prev = 0, index_fpregx_next = 0;
+	unsigned long index = 0, tmp = 0;
 	unsigned long tinstr = 0;
+	u16 instr_hi, instr_low;
 
 	instrptr = instruction_pointer(regs);
+	if (instrptr & 1) return 0;
 
-	/* CSKYV2's 32 bit instruction may not align 4 words */
-	fs = get_fs();
-	set_fs(KERNEL_DS);
-	fault = __get_user(instr_low, (u16 *)(instrptr & ~1));
-	set_fs(fs);
-	if (fault) {
-		goto bad_or_fault;
-	}
+	fault = __get_user(instr_low, (u16 *)instrptr);
+	if (fault) return 0;
 
-	fs = get_fs();
-	set_fs(KERNEL_DS);
-	fault = __get_user(instr_hi, (u16 *)((instrptr + 2) & ~1));
-	set_fs(fs);
-	if (fault) {
-		goto bad_or_fault;
-	}
+	fault = __get_user(instr_hi, (u16 *)(instrptr + 2));
+	if (fault) return 0;
 
 	tinstr = instr_hi | ((unsigned long)instr_low << 16);
 
-	index_fpregx_next = ((tinstr >> 21) & 0x1F);
+	if (((tinstr >> 21) & 0x1F) != 2) return 0;
 
-	/* just want to handle instruction which opration cr<1, 2> or cr<2, 2> */
-	if(index_fpregx_next != 2){
-		goto bad_or_fault;
-	}
-
-	/*
-	 * define four macro to distinguish the instruction is mfcr or mtcr.
-	 */
-#define MTCR_MASK 0xFC00FFE0
-#define MFCR_MASK 0xFC00FFE0
-#define MTCR_DISTI 0xC0006420
-#define MFCR_DISTI 0xC0006020
-
-	if ((tinstr & MTCR_MASK) == MTCR_DISTI)
+	if ((tinstr & MTCR_MASK) == MTCR_DIST)
 	{
-		index_regx = (tinstr >> 16) & 0x1F;
-		index_fpregx_prev = tinstr & 0x1F;
+		index = (tinstr >> 16) & 0x1F;
+		if(index > 13) return 0;
 
-		regx = read_pt_regs(index_regx, regs);
+		tmp = tinstr & 0x1F;
+		if (tmp > 2) return 0;
 
-		if(index_fpregx_prev == 1) {
-			write_fpcr(regx);
-		} else if (index_fpregx_prev == 2) {
-			write_fpesr(regx);
-		} else {
-			goto bad_or_fault;
-		}
+		regx =  *(&regs->a0 + index);
 
-		regs->pc +=4;
-		return 1;
-	} else if ((tinstr & MFCR_MASK) == MFCR_DISTI) {
-		index_regx = tinstr & 0x1F;
-		index_fpregx_prev = ((tinstr >> 16) & 0x1F);
-
-		if (index_fpregx_prev == 1) {
-			regx = read_fpcr();
-		} else if (index_fpregx_prev == 2) {
-			regx = read_fpesr();
-		} else {
-			goto bad_or_fault;
-		}
-
-		write_pt_regs(regx, index_regx, regs);
+		if(tmp == 1)
+			mtcr("cr<1, 2>", regx);
+		else if (tmp == 2)
+			mtcr("cr<2, 2>", regx);
+		else
+			return 0;
 
 		regs->pc +=4;
 		return 1;
 	}
 
-bad_or_fault:
+	if ((tinstr & MFCR_MASK) == MFCR_DIST) {
+		index = tinstr & 0x1F;
+		if(index > 13) return 0;
+
+		tmp = ((tinstr >> 16) & 0x1F);
+		if (tmp > 2) return 0;
+
+		if (tmp == 1)
+			regx = mfcr("cr<1, 2>");
+		else if (tmp == 2)
+			regx = mfcr("cr<2, 2>");
+		else
+			return 0;
+
+		*(&regs->a0 + index) = regx;
+
+		regs->pc +=4;
+		return 1;
+	}
+
 	return 0;
 }
 
@@ -203,30 +132,29 @@ void fpu_fpe(struct pt_regs * regs)
 	force_sig_info(sig, &info, current);
 }
 
-#define FMFVR_FPU_REGS(vrx, vry) \
-	"fmfvrl %0, "#vrx" \n" \
-	"fmfvrh %1, "#vrx" \n" \
-	"fmfvrl %2, "#vry" \n" \
+#define FMFVR_FPU_REGS(vrx, vry)	\
+	"fmfvrl %0, "#vrx" \n"		\
+	"fmfvrh %1, "#vrx" \n"		\
+	"fmfvrl %2, "#vry" \n"		\
 	"fmfvrh %3, "#vry" \n"
 
-#define FMTVR_FPU_REGS(vrx, vry) \
-	"fmtvrl "#vrx", %0 \n" \
-	"fmtvrh "#vrx", %1 \n" \
-	"fmtvrl "#vry", %2 \n" \
+#define FMTVR_FPU_REGS(vrx, vry)	\
+	"fmtvrl "#vrx", %0 \n"		\
+	"fmtvrh "#vrx", %1 \n"		\
+	"fmtvrl "#vry", %2 \n"		\
 	"fmtvrh "#vry", %3 \n"
 
-#define STW_FPU_REGS(a, b, c, d) \
-	"stw    %0, (%4, "#a") \n" \
-	"stw    %1, (%4, "#b") \n" \
-	"stw    %2, (%4, "#c") \n" \
+#define STW_FPU_REGS(a, b, c, d)	\
+	"stw    %0, (%4, "#a") \n"	\
+	"stw    %1, (%4, "#b") \n"	\
+	"stw    %2, (%4, "#c") \n"	\
 	"stw    %3, (%4, "#d") \n"
 
-#define LDW_FPU_REGS(a, b, c, d) \
-	"ldw    %0, (%4, "#a") \n" \
-	"ldw    %1, (%4, "#b") \n" \
-	"ldw    %2, (%4, "#c") \n" \
+#define LDW_FPU_REGS(a, b, c, d)	\
+	"ldw    %0, (%4, "#a") \n"	\
+	"ldw    %1, (%4, "#b") \n"	\
+	"ldw    %2, (%4, "#c") \n"	\
 	"ldw    %3, (%4, "#d") \n"
-
 
 void save_to_user_fp(struct user_fp *user_fp)
 {
