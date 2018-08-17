df669479aa38ccb66e4410120cdb84997781f690
Guo Ren <ren_guo@c-sky.com>
2018-08-17 11:35:34 +0800

    csky: asm/compat.h added
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

---
+++ arch/csky/include/asm/compat.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2018 Hangzhou C-SKY Microsystems co.,ltd.
+
+#ifndef __ASM_CSKY_COMPAT_H
+#define __ASM_CSKY_COMPAT_H
+
+#ifdef CONFIG_COMPAT
+#define COMPAT_UTS_MACHINE "csky\0\0"
+#endif
+
+#endif /* __ASM_CSKY_COMPAT_H */
