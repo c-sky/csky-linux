195dcb667b21c4f44c3911adbb6b9ef2a9ac0891
Guo Ren <ren_guo@c-sky.com>
2019-01-08 20:31:43 +0800

    csky: fixup compile error with pte_alloc
    Commit: 4cf58924951e remove the address argument of pte_alloc without
    modify csky related code. linux-5.0-rc1 compile failed with csky.
    
    Remove the unnecessary address testing in pte_alloc().
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>
    Cc: Joel Fernandes (Google) <joel@joelfernandes.org>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/csky/include/asm/pgalloc.h
+++ arch/csky/include/asm/pgalloc.h
@@ -23,44 +23,35 @@ static inline void pmd_populate(struct mm_struct *mm, pmd_t *pmd,
 
 extern void pgd_init(unsigned long *p);
 
-#if (LINUX_VERSION_CODE >> 8) == (KERNEL_VERSION(4,9,0) >> 8)
-#define __GFP_RETRY_MAYFAIL __GFP_REPEAT
-#endif
-static inline pte_t *pte_alloc_one_kernel(struct mm_struct *mm,
-        unsigned long address)
+static inline pte_t *pte_alloc_one_kernel(struct mm_struct *mm, unsigned long addr)
 {
-        pte_t *pte;
-	unsigned long *kaddr, i;
+	pte_t *pte;
+	unsigned long i;
 
-	pte = (pte_t *) __get_free_pages(GFP_KERNEL | __GFP_RETRY_MAYFAIL, PTE_ORDER);
-	kaddr = (unsigned long *)pte;
-	if (address & 0x80000000)
-		for(i=0; i<(PAGE_SIZE/4); i++)
-			*(kaddr + i) = 0x1;
-	else
-		clear_page(kaddr);
+	pte = (pte_t *) __get_free_page(GFP_KERNEL);
+	if (!pte)
+		return NULL;
 
-        return pte;
+	for (i = 0; i < PAGE_SIZE/sizeof(pte_t); i++)
+		(pte + i)->pte_low = _PAGE_GLOBAL;
+
+	return pte;
 }
 
-static inline struct page *pte_alloc_one(struct mm_struct *mm,
-                                            unsigned long address)
+static inline struct page *pte_alloc_one(struct mm_struct *mm, unsigned long addr)
 {
 	struct page *pte;
-	unsigned long *kaddr, i;
 
-        pte = alloc_pages(GFP_KERNEL | __GFP_RETRY_MAYFAIL, PTE_ORDER);
-        if (pte) {
-		kaddr = kmap_atomic(pte);
-		if (address & 0x80000000) {
-			for(i=0; i<(PAGE_SIZE/4); i++)
-				*(kaddr + i) = 0x1;
-		} else
-			clear_page(kaddr);
-		kunmap_atomic(kaddr);
-		pgtable_page_ctor(pte);
-        }
-        return pte;
+	pte = alloc_pages(GFP_KERNEL | __GFP_ZERO, 0);
+	if (!pte)
+		return NULL;
+
+	if (!pgtable_page_ctor(pte)) {
+		__free_page(pte);
+		return NULL;
+	}
+
+	return pte;
 }
 
 static inline void pte_free_kernel(struct mm_struct *mm, pte_t *pte)
