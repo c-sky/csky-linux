2ee596fb4e5ccd9d6ee3027102c6054a665580fe
Guo Ren <ren_guo@c-sky.com>
2019-01-30 20:13:11 +0800

    csky: Fixup io-range page attribute for mmap("/dev/mem")
    Some user space drivers need accessing IO address and IO remap need
    SO(strong order) page-attribute to make IO operation correct. So we
    need add SO-page-attr for all non-memory address.
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>
    Reported-by: Fan Xiaodong <xiaodong.fan@boyahualu.com>

--- arch/csky/mm/ioremap.c
+++ arch/csky/mm/ioremap.c
@@ -47,3 +47,16 @@ void iounmap(void __iomem *addr)
 }
 EXPORT_SYMBOL(iounmap);
 
+pgprot_t phys_mem_access_prot(struct file *file, unsigned long pfn,
+			      unsigned long size, pgprot_t vma_prot)
+{
+	if (!pfn_valid(pfn)) {
+		vma_prot.pgprot |= _PAGE_SO;
+		return pgprot_noncached(vma_prot);
+	} else if (file->f_flags & O_SYNC) {
+		return pgprot_noncached(vma_prot);
+	}
+
+	return vma_prot;
+}
+EXPORT_SYMBOL(phys_mem_access_prot);
