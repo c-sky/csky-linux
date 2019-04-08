c3d5c599e08bf53b975397befe9dd60816f542cc
Guo Ren <ren_guo@c-sky.com>
2019-04-08 11:12:25 +0800

    csky: Support dynamic start physical address
    Before this patch csky-linux need CONFIG_RAM_BASE to determine start
    physical address. Now we use phys_offset variable to replace the macro
    of PHYS_OFFSET and we setup phys_offset with real physical address which
    is determined during startup in head.S.
    
    With this patch we needn't re-compile kernel for different start
    physical address. ie: 0x0 / 0xc0000000 start physical address could use
    the same vmlinux, be care different start address must be 512MB aligned.
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>
    Cc: Arnd Bergmann <arnd@arndb.de>

--- arch/csky/include/asm/page.h
+++ arch/csky/include/asm/page.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
 #ifndef __ASM_CSKY_PAGE_H
 #define __ASM_CSKY_PAGE_H
 
@@ -6,21 +8,27 @@
 #include <linux/const.h>
 
 /*
- * PAGE_SHIFT determines the page size
+ * PAGE_SHIFT determines the page size: 4KB
  */
 #define PAGE_SHIFT	12
 #define PAGE_SIZE	(_AC(1, UL) << PAGE_SHIFT)
 #define PAGE_MASK	(~(PAGE_SIZE - 1))
 #define THREAD_SIZE	(PAGE_SIZE * 2)
-#define THREAD_MASK	(~(THREAD_SIZE -1))
+#define THREAD_MASK	(~(THREAD_SIZE - 1))
 #define THREAD_SHIFT	(PAGE_SHIFT + 1)
 
+
 /*
- * NOTE: virtual isn't really correct, actually it should be the offset into the
- * memory node, but we have no highmem, so that works for now.
- * TODO: implement (fast) pfn<->pgdat_idx conversion functions, this makes lots
- * of the shifts unnecessary.
+ * For C-SKY "User-space:Kernel-space" is "2GB:2GB" fixed by hardware and there
+ * are two segment registers (MSA0 + MSA1) to mapping 512MB + 512MB physical
+ * address region. We use them mapping kernel 1GB direct-map address area and
+ * for more than 1GB of memory we use highmem.
  */
+#define PAGE_OFFSET	0x80000000
+#define SSEG_SIZE	0x20000000
+#define LOWMEM_LIMIT	(SSEG_SIZE * 2)
+
+#define PHYS_OFFSET_OFFSET (CONFIG_RAM_BASE & (SSEG_SIZE - 1))
 
 #ifndef __ASSEMBLY__
 
@@ -30,14 +38,14 @@
 #define pfn_to_virt(pfn)        __va((pfn) << PAGE_SHIFT)
 
 #define virt_addr_valid(kaddr)  ((void *)(kaddr) >= (void *)PAGE_OFFSET && \
-                                 (void *)(kaddr) < high_memory)
+			(void *)(kaddr) < high_memory)
 #define pfn_valid(pfn)		((pfn) >= ARCH_PFN_OFFSET && ((pfn) - ARCH_PFN_OFFSET) < max_mapnr)
 
 extern void *memset(void *dest, int c, size_t l);
-extern void *memcpy (void *to, const void *from, size_t l);
+extern void *memcpy(void *to, const void *from, size_t l);
 
-#define clear_page(page)        memset((page), 0, PAGE_SIZE)
-#define copy_page(to,from)      memcpy((to), (from), PAGE_SIZE)
+#define clear_page(page)	memset((page), 0, PAGE_SIZE)
+#define copy_page(to, from)	memcpy((to), (from), PAGE_SIZE)
 
 #define page_to_phys(page)	(page_to_pfn(page) << PAGE_SHIFT)
 #define phys_to_page(paddr)	(pfn_to_page(PFN_DOWN(paddr)))
@@ -48,11 +56,8 @@ struct page;
 
 struct vm_area_struct;
 
-/*
- * These are used to make use of C type-checking..
- */
 typedef struct { unsigned long pte_low; } pte_t;
-#define pte_val(x)    ((x).pte_low)
+#define pte_val(x)	((x).pte_low)
 
 typedef struct { unsigned long pgd; } pgd_t;
 typedef struct { unsigned long pgprot; } pgprot_t;
@@ -63,38 +68,30 @@ typedef struct page *pgtable_t;
 
 #define ptep_buddy(x)	((pte_t *)((unsigned long)(x) ^ sizeof(pte_t)))
 
-#define __pte(x)	((pte_t) { (x) } )
-#define __pgd(x)	((pgd_t) { (x) } )
-#define __pgprot(x)	((pgprot_t) { (x) } )
+#define __pte(x)	((pte_t) { (x) })
+#define __pgd(x)	((pgd_t) { (x) })
+#define __pgprot(x)	((pgprot_t) { (x) })
 
-#endif /* !__ASSEMBLY__ */
+extern unsigned long phys_offset;
 
-#define PHYS_OFFSET		(CONFIG_RAM_BASE & ~(LOWMEM_LIMIT - 1))
-#define PHYS_OFFSET_OFFSET	(CONFIG_RAM_BASE & (LOWMEM_LIMIT - 1))
-#define ARCH_PFN_OFFSET		PFN_DOWN(CONFIG_RAM_BASE)
+#define ARCH_PFN_OFFSET	PFN_DOWN(phys_offset + PHYS_OFFSET_OFFSET)
 
-#define	PAGE_OFFSET	0x80000000
-#define LOWMEM_LIMIT	0x40000000
+#define __pa(x)		((unsigned long)(x) - PAGE_OFFSET + phys_offset)
+#define __va(x)		((void *)((unsigned long)(x) + PAGE_OFFSET - \
+				  phys_offset))
+#define __pa_symbol(x)	__pa(RELOC_HIDE((unsigned long)(x), 0))
 
-#define __pa(x)		((unsigned long)(x) - PAGE_OFFSET + PHYS_OFFSET)
-#define __va(x)		((void *)((unsigned long)(x) + PAGE_OFFSET - PHYS_OFFSET))
-#define __pa_symbol(x)  __pa(RELOC_HIDE((unsigned long)(x), 0))
-
-#define MAP_NR(x)	PFN_DOWN((unsigned long)(x) - PAGE_OFFSET - PHYS_OFFSET_OFFSET)
-#define virt_to_page(x)		(mem_map + MAP_NR(x))
+#define MAP_NR(x)	PFN_DOWN((unsigned long)(x) - PAGE_OFFSET - \
+				 PHYS_OFFSET_OFFSET)
+#define virt_to_page(x)	(mem_map + MAP_NR(x))
 
 #define VM_DATA_DEFAULT_FLAGS	(VM_READ | VM_WRITE | VM_EXEC | \
-				 VM_MAYREAD | VM_MAYWRITE | VM_MAYEXEC)
+				VM_MAYREAD | VM_MAYWRITE | VM_MAYEXEC)
 
-/*
- * main RAM and kernel working space are coincident at 0x80000000, but to make
- * life more interesting, there's also an uncached virtual shadow at 0xb0000000
- * - these mappings are fixed in the MMU
- */
-
-#define pfn_to_kaddr(x)       __va(PFN_PHYS(x))
+#define pfn_to_kaddr(x)	__va(PFN_PHYS(x))
 
 #include <asm-generic/memory_model.h>
 #include <asm-generic/getorder.h>
 
+#endif /* !__ASSEMBLY__ */
 #endif /* __ASM_CSKY_PAGE_H */
--- arch/csky/include/asm/pgalloc.h
+++ arch/csky/include/asm/pgalloc.h
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 */
 // Copyright (C) 2018 Hangzhou C-SKY Microsystems co.,ltd.
+
 #ifndef __ASM_CSKY_PGALLOC_H
 #define __ASM_CSKY_PGALLOC_H
 
@@ -8,15 +9,15 @@
 #include <linux/sched.h>
 
 static inline void pmd_populate_kernel(struct mm_struct *mm, pmd_t *pmd,
-        pte_t *pte)
+					pte_t *pte)
 {
-        set_pmd(pmd, __pmd(__pa(pte)));
+	set_pmd(pmd, __pmd(__pa(pte)));
 }
 
 static inline void pmd_populate(struct mm_struct *mm, pmd_t *pmd,
-        pgtable_t pte)
+					pgtable_t pte)
 {
-        set_pmd(pmd, __pmd(__pa(page_address(pte))));
+	set_pmd(pmd, __pmd(__pa(page_address(pte))));
 }
 
 #define pmd_pgtable(pmd) pmd_page(pmd)
@@ -56,48 +57,52 @@ static inline struct page *pte_alloc_one(struct mm_struct *mm, unsigned long add
 
 static inline void pte_free_kernel(struct mm_struct *mm, pte_t *pte)
 {
-        free_pages((unsigned long)pte, PTE_ORDER);
+	free_pages((unsigned long)pte, PTE_ORDER);
 }
 
 static inline void pte_free(struct mm_struct *mm, pgtable_t pte)
 {
-        pgtable_page_dtor(pte);
-        __free_pages(pte, PTE_ORDER);
+	pgtable_page_dtor(pte);
+	__free_pages(pte, PTE_ORDER);
 }
 
 static inline void pgd_free(struct mm_struct *mm, pgd_t *pgd)
 {
-        free_pages((unsigned long)pgd, PGD_ORDER);
+	free_pages((unsigned long)pgd, PGD_ORDER);
 }
 
 static inline pgd_t *pgd_alloc(struct mm_struct *mm)
 {
-        pgd_t *ret, *init;
+	pgd_t *ret;
+	pgd_t *init;
 
-        ret = (pgd_t *) __get_free_pages(GFP_KERNEL, PGD_ORDER);
-        if (ret) {
-                init = pgd_offset(&init_mm, 0UL);
-                pgd_init((unsigned long *)ret);
-                memcpy(ret + USER_PTRS_PER_PGD, init + USER_PTRS_PER_PGD,
-                       (PTRS_PER_PGD - USER_PTRS_PER_PGD) * sizeof(pgd_t));
+	ret = (pgd_t *) __get_free_pages(GFP_KERNEL, PGD_ORDER);
+	if (ret) {
+		init = pgd_offset(&init_mm, 0UL);
+		pgd_init((unsigned long *)ret);
+		memcpy(ret + USER_PTRS_PER_PGD, init + USER_PTRS_PER_PGD,
+			(PTRS_PER_PGD - USER_PTRS_PER_PGD) * sizeof(pgd_t));
+		/* prevent out of order excute */
 		smp_mb();
 #ifdef CONFIG_CPU_NEED_TLBSYNC
-		dcache_wb_range((unsigned int)ret, (unsigned int)(ret + PTRS_PER_PGD));
+		dcache_wb_range((unsigned int)ret,
+				(unsigned int)(ret + PTRS_PER_PGD));
 #endif
-        }
+	}
 
-        return ret;
+	return ret;
 }
 
-#define __pte_free_tlb(tlb,pte,address)                 \
-do {                                                    \
-        pgtable_page_dtor(pte);                         \
-        tlb_remove_page((tlb), pte);                    \
+#define __pte_free_tlb(tlb, pte, address)		\
+do {							\
+	pgtable_page_dtor(pte);				\
+	tlb_remove_page(tlb, pte);			\
 } while (0)
 
-#define check_pgt_cache()               do {} while(0)
+#define check_pgt_cache()	do {} while (0)
 
 extern void pagetable_init(void);
+extern void pre_mmu_init(void);
+extern void pre_trap_init(void);
 
 #endif /* __ASM_CSKY_PGALLOC_H */
-
