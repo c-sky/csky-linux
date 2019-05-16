da105e26a9e81f56ab679042a1f32101a721b0ba
Guo Ren <ren_guo@c-sky.com>
2019-05-16 11:52:13 +0800

    csky: Support dma<->phys with dev=NULL param
    Some drivers just pass dev=NULL for first param and we give support for
    them. But it can not be upstreamed into mainline linux, because dev is
    necessary for this function :P
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>
    Reported-by: Zhang Jian <zhang_jian5@dahuatech.com>

--- arch/csky/include/asm/dma-mapping.h
+++ arch/csky/include/asm/dma-mapping.h
@@ -22,12 +22,18 @@ static inline dma_addr_t phys_to_dma(struct device *dev, phys_addr_t paddr)
 {
 	dma_addr_t dev_addr = (dma_addr_t)paddr;
 
+	if (dev == NULL)
+		return dev_addr;
+
 	return dev_addr - ((dma_addr_t)dev->dma_pfn_offset << PAGE_SHIFT);
 }
 
 static inline phys_addr_t dma_to_phys(struct device *dev, dma_addr_t dev_addr)
 {
 	phys_addr_t paddr = (phys_addr_t)dev_addr;
+
+	if (dev == NULL)
+		return paddr;
 
 	return paddr + ((phys_addr_t)dev->dma_pfn_offset << PAGE_SHIFT);
 }
