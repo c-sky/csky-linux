75a41a4ef31224dd69f60115ec63cd953d4d4a3f
Guo Ren <guoren@linux.alibaba.com>
2020-02-02 10:58:38 +0800

    csky: Remove unused cache implementation
    Only for coding convention, these codes are unnecessary for abiv2.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>

--- arch/csky/mm/cachev2.c
+++ arch/csky/mm/cachev2.c
@@ -60,12 +60,7 @@ void dcache_wbinv_all(void)
 
 void cache_wbinv_range(unsigned long start, unsigned long end)
 {
-	unsigned long i = start & ~(L1_CACHE_BYTES - 1);
-
-	for (; i < end; i += L1_CACHE_BYTES)
-		asm volatile("dcache.cval1 %0\n"::"r"(i):"memory");
-	sync_is();
-
+	dcache_wb_range(start, end);
 	icache_inv_range(start, end);
 }
 EXPORT_SYMBOL(cache_wbinv_range);
