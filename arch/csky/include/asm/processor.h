1cd60acf86a9e995d1843682befb93f48336ac8d
Guo Ren <guoren@linux.alibaba.com>
2020-05-14 16:04:31 +0800

    csky: Fixup gdbmacros.txt with name sp in thread_struct
    The gdbmacros.txt use sp in thread_struct, but csky use ksp. This
    cause bttnobp fail to excute.
    
    TODO:
     - Still couldn't display the contents of stack.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>

--- arch/csky/include/asm/processor.h
+++ arch/csky/include/asm/processor.h
@@ -46,7 +46,7 @@ extern struct cpuinfo_csky cpu_data[];
 #define TASK_UNMAPPED_BASE      (TASK_SIZE / 3)
 
 struct thread_struct {
-	unsigned long  ksp;       /* kernel stack pointer */
+	unsigned long  sp;        /* kernel stack pointer */
 	unsigned long  trap_no;   /* saved status register */
 
 	/* FPU regs */
@@ -54,7 +54,7 @@ struct thread_struct {
 };
 
 #define INIT_THREAD  { \
-	.ksp = sizeof(init_stack) + (unsigned long) &init_stack, \
+	.sp = sizeof(init_stack) + (unsigned long) &init_stack, \
 }
 
 /*
