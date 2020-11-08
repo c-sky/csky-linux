cb6fb0d2d700baacba75d5fb297c16c489a67693
Guo Ren <guoren@linux.alibaba.com>
2020-11-08 03:59:54 +0000

    csky: Add QUEUED_SPINLOCKS supported
    Abiv2 only support ldex.w/stex.w with word size and word align. So,
    there are no ldex.h/stex.h and ldex.b/stex.b instructions. But
    qspinlock.c need xchg with half word:
    xchg_tail -> xchg_releaxed(&lock->tail, ...
    typedef struct qspinlock {
            union {
                    atomic_t val;
    
                    /*
                     * By using the whole 2nd least significant byte for the
                     * pending bit, we can allow better optimization of the lock
                     * acquisition for the pending bit holder.
                     */
                    struct {
                            u8      locked;
                            u8      pending;
                    };
                    struct {
                            u16     locked_pending;
                            u16     tail; /* half word*/
                    };
            };
    } arch_spinlock_t;
    
    So we add halfword emulation in xchg for csky and only solve
    qspinlock's usage.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Cc: Peter Zijlstra <peterz@infradead.org>

--- arch/csky/include/asm/cmpxchg.h
+++ arch/csky/include/asm/cmpxchg.h
@@ -12,9 +12,46 @@ extern void __bad_xchg(void);
 ({								\
 	__typeof__(ptr) __ptr = (ptr);				\
 	__typeof__(new) __new = (new);				\
-	__typeof__(*(ptr)) __ret;				\
-	unsigned long tmp;					\
+	__typeof__(*(ptr)) __ret = 0;				\
+	unsigned long tmp, tmp2, align, addr;			\
 	switch (size) {						\
+	case 2:							\
+		align = ((unsigned long) __ptr & 0x3);		\
+		addr = ((unsigned long) __ptr & ~0x3);		\
+		smp_mb();					\
+		if (align) {					\
+		asm volatile (					\
+		"1:	ldex.w		%0, (%4) \n"		\
+		"	mov		%1, %0   \n"		\
+		"	lsli		%1, 16   \n"		\
+		"	lsri		%1, 16   \n"		\
+		"	mov		%2, %3   \n"		\
+		"	lsli		%2, 16   \n"		\
+		"	or		%1, %2   \n"		\
+		"	stex.w		%1, (%4) \n"		\
+		"	bez		%1, 1b   \n"		\
+		"	lsri		%0, 16   \n"		\
+			: "=&r" (__ret), "=&r" (tmp),		\
+			  "=&r" (tmp2)				\
+			: "r" (__new), "r"(addr)		\
+			:);					\
+		} else {					\
+		asm volatile (					\
+		"1:	ldex.w		%0, (%3) \n"		\
+		"	mov		%1, %0   \n"		\
+		"	lsri		%1, 16   \n"		\
+		"	lsli		%1, 16   \n"		\
+		"	or		%1, %2   \n"		\
+		"	stex.w		%1, (%3) \n"		\
+		"	bez		%1, 1b   \n"		\
+		"	lsli		%0, 16   \n"		\
+		"	lsri		%0, 16   \n"		\
+			: "=&r" (__ret), "=&r" (tmp)		\
+			: "r" (__new), "r"(addr)		\
+			:);					\
+		}						\
+		smp_mb();					\
+		break;						\
 	case 4:							\
 		smp_mb();					\
 		asm volatile (					\
@@ -41,7 +78,7 @@ extern void __bad_xchg(void);
 	__typeof__(new) __new = (new);				\
 	__typeof__(new) __tmp;					\
 	__typeof__(old) __old = (old);				\
-	__typeof__(*(ptr)) __ret;				\
+	__typeof__(*(ptr)) __ret = 0;				\
 	switch (size) {						\
 	case 4:							\
 		smp_mb();					\
