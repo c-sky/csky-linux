bbd66952d31bb3b30b19bc7421ce187f9bb9527e
Guo Ren <guoren@linux.alibaba.com>
2021-12-13 21:16:32 +0800

    riscv: compat: vdso: Add rv32 VDSO base code implementation
    There is no vgettimeofday supported in rv32 that makes simple to
    generate rv32 vdso code which only needs riscv64 compiler. Other
    architectures need change compiler or -m (machine parameter) to
    support vdso32 compiling. If rv32 support vgettimeofday (which
    cause C compile) in future, we would add CROSS_COMPILE to support
    that makes more requirement on compiler enviornment.
    
    linux-rv64/arch/riscv/kernel/compat_vdso/compat_vdso.so.dbg:
    file format elf64-littleriscv
    
    Disassembly of section .text:
    
    0000000000000800 <__vdso_rt_sigreturn>:
     800:   08b00893                li      a7,139
     804:   00000073                ecall
     808:   0000                    unimp
            ...
    
    000000000000080c <__vdso_getcpu>:
     80c:   0a800893                li      a7,168
     810:   00000073                ecall
     814:   8082                    ret
            ...
    
    0000000000000818 <__vdso_flush_icache>:
     818:   10300893                li      a7,259
     81c:   00000073                ecall
     820:   8082                    ret
    
    linux-rv32/arch/riscv/kernel/vdso/vdso.so.dbg:
    file format elf32-littleriscv
    
    Disassembly of section .text:
    
    00000800 <__vdso_rt_sigreturn>:
     800:   08b00893                li      a7,139
     804:   00000073                ecall
     808:   0000                    unimp
            ...
    
    0000080c <__vdso_getcpu>:
     80c:   0a800893                li      a7,168
     810:   00000073                ecall
     814:   8082                    ret
            ...
    
    00000818 <__vdso_flush_icache>:
     818:   10300893                li      a7,259
     81c:   00000073                ecall
     820:   8082                    ret
    
    Finally, reuse all *.S from vdso in compat_vdso that makes
    implementation clear and readable.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>

--- arch/riscv/include/asm/vdso.h
+++ arch/riscv/include/asm/vdso.h
@@ -19,6 +19,14 @@ struct vdso_data {
 #define VDSO_SYMBOL(base, name)							\
 	(void __user *)((unsigned long)(base) + __vdso_##name##_offset)
 
+#ifdef CONFIG_COMPAT
+#include <generated/compat_vdso-offsets.h>
+
+#define COMPAT_VDSO_SYMBOL(base, name)						\
+	(void __user *)((unsigned long)(base) + compat__vdso_##name##_offset)
+
+#endif /* CONFIG_COMPAT */
+
 asmlinkage long sys_riscv_flush_icache(uintptr_t, uintptr_t, uintptr_t);
 
 #endif /* _ASM_RISCV_VDSO_H */
