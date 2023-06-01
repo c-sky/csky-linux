5a248bb3f14866891dcde800402f4561451699a9
Guo Ren <guoren@linux.alibaba.com>
2023-06-01 02:21:02 -0400

    riscv: s64ilp32: Adjust TASK_SIZE for s64ilp32 kernel
    The RV64ILP32 32-bit Linux kernel uses the same userspace address range
    as the 64-bit Linux compat mode, about 2GB. They have no difference from
    the hardware view, and all are running ILP32 on a 64-bit ISA. But the
    standard 32ilp32 Linux has a slightly bigger userspace address space,
    about 2.4GB.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>

--- arch/riscv/include/asm/pgtable.h
+++ arch/riscv/include/asm/pgtable.h
@@ -833,18 +833,20 @@ static inline pte_t pte_swp_clear_exclusive(pte_t pte)
  * -     0x9fc00000 (~2.5GB) for RV32.
  * -   0x4000000000 ( 256GB) for RV64 using SV39 mmu
  * - 0x800000000000 ( 128TB) for RV64 using SV48 mmu
+ * -     0x80000000 (   2GB) for COMPAT and RV64ILP32
  *
  * Note that PGDIR_SIZE must evenly divide TASK_SIZE since "RISC-V
  * Instruction Set Manual Volume II: Privileged Architecture" states that
  * "load and store effective addresses, which are 64bits, must have bits
  * 63–48 all equal to bit 47, or else a page-fault exception will occur."
  */
+#define TASK_SIZE_32	(_AC(0x80000000, UL) - PAGE_SIZE)
+
 #ifdef CONFIG_64BIT
 #define TASK_SIZE_64	(PGDIR_SIZE * PTRS_PER_PGD / 2)
 #define TASK_SIZE_MIN	(PGDIR_SIZE_L3 * PTRS_PER_PGD / 2)
 
 #ifdef CONFIG_COMPAT
-#define TASK_SIZE_32	(_AC(0x80000000, UL) - PAGE_SIZE)
 #define TASK_SIZE	(test_thread_flag(TIF_32BIT) ? \
 			 TASK_SIZE_32 : TASK_SIZE_64)
 #else
@@ -852,7 +854,11 @@ static inline pte_t pte_swp_clear_exclusive(pte_t pte)
 #endif
 
 #else
+#ifdef CONFIG_ARCH_RV64ILP32
+#define TASK_SIZE	TASK_SIZE_32
+#else
 #define TASK_SIZE	FIXADDR_START
+#endif
 #define TASK_SIZE_MIN	TASK_SIZE
 #endif
 
