c97299f1eb32bf1347cbed0bed6dd05f8fe2f3d4
Xiaoguang Xing <xiaoguang.xing@sophgo.com>
2023-07-12 11:33:44 +0800

    riscv: include: Add smp_cond_load_acquire()
    Fix qspinlock issue that loops to call cpu_relax and not exit.
    The call trace is:
    queued_spin_lock_slowpath->arch_mcs_spin_lock_contended
    ->smp_cond_load_acquire.
    
    RISCV has not defined smp_cond_load_acquire, so it uses generic
    funtion that defined in include/asm-generic/barrier.h.
    The generic smp_cond_load_acquire calls smp_cond_load_relaxed that
    loops to call READ_ONCE and cpu_relax.
    The READ_ONCE need barrier after it to get the new value.
    
    Signed-off-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>

--- arch/riscv/include/asm/barrier.h
+++ arch/riscv/include/asm/barrier.h
@@ -44,6 +44,18 @@ do {									\
 	___p1;								\
 })
 
+#define smp_cond_load_acquire(ptr, cond_expr) ({			\
+	typeof(ptr) __PTR = (ptr);					\
+	__unqual_scalar_typeof(*ptr) VAL;				\
+	for (;;) {							\
+		VAL = __smp_load_acquire(__PTR);			\
+		if (cond_expr)						\
+			break;						\
+		cpu_relax();						\
+	}								\
+	(typeof(*ptr))VAL;						\
+})
+
 /*
  * This is a very specific barrier: it's currently only used in two places in
  * the kernel, both in the scheduler.  See include/linux/spinlock.h for the two
