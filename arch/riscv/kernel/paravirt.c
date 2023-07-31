9bdf4d656433b3dafd68b2e4636416003adfb61d
Guo Ren <guoren@linux.alibaba.com>
2023-07-31 17:47:44 +0800

    RISC-V: paravirt: pvqspinlock: Add nopvspin kernel parameter
    Disables the qspinlock slow path using PV optimizations which
    allow the hypervisor to 'idle' the guest on lock contention.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>

--- arch/riscv/kernel/paravirt.c
+++ arch/riscv/kernel/paravirt.c
@@ -166,8 +166,21 @@ EXPORT_STATIC_CALL(pv_queued_spin_lock_slowpath);
 DEFINE_STATIC_CALL(pv_queued_spin_unlock, native_queued_spin_unlock);
 EXPORT_STATIC_CALL(pv_queued_spin_unlock);
 
+static bool nopvspin;
+static __init int parse_nopvspin(char *arg)
+{
+       nopvspin = true;
+       return 0;
+}
+early_param("nopvspin", parse_nopvspin);
+
 void __init pv_qspinlock_init(void)
 {
+	if (nopvspin) {
+		pr_info("PV qspinlocks disabled\n");
+		return;
+	}
+
 	if (num_possible_cpus() == 1)
 		return;
 
